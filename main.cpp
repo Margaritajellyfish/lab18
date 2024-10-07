bac8c9bfc4acf6b191f249869d1b529cc907db70
王辰毓 <margarita@MacBook-Pro-3.local>
2024-10-06 18:49:41 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 struct Node{
-
+Node * next = nullptr;
 };
 
-main(){
-
+int main(){
+    int rate;
+    while(loop=t){int choice;
+    
+    cout << "Which linked list method should we use?\n [1] New nodes are added at the head of the linked list\n
+    [2] New nodes are added at the tail of the linked list\nChoice: ";
+    cin >> choice;
+    cout << "Enter review rating 0-5: ";
+    cin >> rate;
+    cout << "Enter review comments: ";
+    cin 
+    }
+    return 0;
 }
